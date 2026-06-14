30e869d7cb009fd3c0c7670bbdd7a2eb898a9142
McJty <mcjty1@gmail.com>
2026-06-14 08:34:33 +0200

    Various Patchouli documentation fixes and upates

--- src/main/java/mcjty/xnet/modules/wireless/blocks/TileEntityWirelessRouter.java
+++ src/main/java/mcjty/xnet/modules/wireless/blocks/TileEntityWirelessRouter.java
@@ -84,7 +84,7 @@ public final class TileEntityWirelessRouter extends TickingTileEntity {
         return new BaseBlock(new BlockBuilder()
                 .topDriver(XNetTOPDriver.DRIVER)
                 .tileEntitySupplier(TileEntityWirelessRouter::new)
-                .manualEntry(ManualHelper.create("xnet:simple/wireless/wireless_router"))
+                .manualEntry(ManualHelper.create("xnet:wireless/wireless_router"))
                 .info(TooltipBuilder.key("message.xnet.shiftmessage"))
                 .infoShift(TooltipBuilder.header(), TooltipBuilder.gold())
         ) {
